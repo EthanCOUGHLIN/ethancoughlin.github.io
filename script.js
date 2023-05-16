07c84975ffd66457717824c1805c36f41293bb32
EthanCOUGHLIN <126134359+EthanCOUGHLIN@users.noreply.github.com>
2023-05-16 13:21:16 +1000

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction()
+{
+
+}
+
+function Disappear() {
+    document.getElementById('myDiv').style.display = 'none';
+}
+
+function Reappear() {
+    document.getElementById('myDiv').style.display = 'block';
+}
+
+function click() {
+    document.getElementById('myDiv').style.display = 'block'
+}
