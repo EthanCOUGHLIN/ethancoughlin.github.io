c51570341b7c55937eed7ef3625a6effe84d83ca
EthanCOUGHLIN <126134359+EthanCOUGHLIN@users.noreply.github.com>
2023-05-22 09:54:28 +1000

    update

--- script.js
+++ script.js
@@ -20,4 +20,22 @@ function myName() {
     document.getElementById('myDiv').innerHTML = name;
 }
 
+function click() {
+    let name = "1994";
+    document.getElementById('myDiv2').innerHTML = name;
+}
+
+alert ("Ayyyyyyyyyyyyyyyyyyyyy")
+
+confirm ("was Super Metroid was released in 1994?")
+
+let person = prompt("INPUT NAME NOW DAMMIT!", "");
+let text;
+if (person == null || person == "") {
+    text = "User cancelled the prompt";
+} else {
+    text = "Ayyyyyyyyyyyyy" + person + "! Did you know that Super Metroid was released in 1994";
+}
+
+
 
