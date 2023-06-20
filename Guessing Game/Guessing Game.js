bc914fd4b6ee86e87ba74bee8b4ab7829f11305e
EthanCOUGHLIN <126134359+EthanCOUGHLIN@users.noreply.github.com>
2023-06-20 14:11:06 +1000

    update

--- Guessing Game/Guessing Game.js
+++ Guessing Game/Guessing Game.js
@@ -1,8 +1,30 @@
-
+var guesses = 10;
 var randomNum = getRndInteger(1, 100)
-document.getElementById('').innerHTML = randomNum;
+console.log(randomNum);
 
-let (value = 10)
-if (value = 0) text="Game Over"
+//document.getElementById('').innerHTML = randomNum;
 
-(10-=1)
+//let (value = 10)
+//if (value = 0) text="Game Over"
+
+//(10-=1)
+
+function myChange()
+{
+    let userGuess = document.getElementById('fname').value;
+    console.log(userGuess);
+    if(guesses <= 0) {
+        alert("You lose")
+        return
+    } 
+    
+    guesses--;
+    document.getElementById("guesses").innerHTML = "Guesses - " + guesses;
+}
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
